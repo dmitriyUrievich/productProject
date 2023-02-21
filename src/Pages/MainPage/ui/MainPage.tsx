575ca639e9fcd4339459b0d3a2ad677b475dafe1
dmitriyUrievich <dmitriy.bubenin@gmail.com>
2023-02-21 13:42:43 +0300

    fix AboutPagee

--- src/Pages/MainPage/ui/MainPage.tsx
+++ src/Pages/MainPage/ui/MainPage.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { BugButton } from 'app/providers/ErrorBoundary'
 
-const MainPage = () => {
-    const { t } = useTranslation('main')
-    return (
-        <div>
-            {t('Главная')}
-            <BugButton />
-        </div>
-    )
+interface MainPageProps {
+  className?: string
+}
+
+const MainPage = ({ className }: MainPageProps) => {
+  const { t } = useTranslation('main')
+  return (
+    <div>
+      {t('Главная')}
+      <BugButton />
+    </div>
+  )
 }
 
 export default MainPage
--- src/Pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -3,14 +3,14 @@ import { useTranslation } from 'react-i18next'
 import cls from './NotFoundPage.module.scss'
 
 interface NoteFoundPageProps {
-    className?: string;
+  className?: string
 }
 
-export const NotFoundPage = ({ className }:NoteFoundPageProps) => {
-    const { t } = useTranslation()
-    return (
-        <div className={classNames(cls.NotFoundPage, {}, [className])}>
-            {t('Страница не найдена')}
-        </div>
-    )
+export const NotFoundPage = ({ className }: NoteFoundPageProps) => {
+  const { t } = useTranslation()
+  return (
+    <div className={classNames(cls.NotFoundPage, {}, [className])}>
+      {t('Страница не найдена')}
+    </div>
+  )
 }
